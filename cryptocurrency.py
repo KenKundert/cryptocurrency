76cb5ab8e7c2e7a6f06318d59055774a11c8cf4e
Ken Kundert <ken@theKunderts.net>
2021-05-10 12:45:23 -0700

    various enhancements

--- cryptocurrency.py
+++ cryptocurrency.py
@@ -1,21 +1,30 @@
 from quantiphy import Quantity, UnitConversion, UnknownConversion
-from inform import warn, comment, cull
+from inform import Error, warn, comment, cull, conjoin, plural
 
 __version__ = '0.1.0'
 __released__ = '2019-07-12'
 
 
+class Dollars(Quantity):
+    units = '$'
+    form = 'fixed'
+    prec = 2
+    show_commas = True
+    strip_zeros = False
+
 class Currency:
     def __init__(self, tokens, price=0):
         self.tokens = Quantity(tokens, self.UNITS)
-        self.price = Quantity(price, '$')
+        if price < 0:
+            raise Error('price must not be negative.', culprit=self.name())
+        self.price = Dollars(price)
 
     def in_tokens(self):
         return self.tokens
 
     def in_dollars(self, price=None):
         price = self.price if price is None else price
-        return Quantity(self.tokens * price, '$')
+        return Dollars(self.tokens * price)
 
     def name(self):
         return self.__class__.__name__
@@ -92,6 +101,12 @@ class EOS(Currency):
     SYMBOL = 'Ȅ'
     one = Quantity(1, UNITS)
 
+class XLM(Currency):
+    UNITS = 'XLM'
+    NAME = 'Stellar Lumens'
+    SYMBOL = '*'
+    one = Quantity(1, UNITS)
+
 class ZEC(Currency):
     UNITS = 'ZEC'
     NAME = 'Zcash'
@@ -165,37 +180,200 @@ class Account:
     def __init__(self, owner, default=True):
         self.owner = owner
         self.default = default
-        self.transactions = []
+        self.transactions = {}
         accounts[owner] = self
         self.totals = {}     # total value in dollars by token
         self.costs = {}      # total cost in dollars by token
         self.purchased = {}  # number of tokens actually purchased by token
 
     class Transaction:
-        def __init__(self, tokens, date, comment):
+        def __init__(self, tokens, date, comment, fees, source):
             self.tokens = tokens
+            self.proceeds = tokens.in_dollars()
             self.date = date
-            self.cost = tokens.in_dollars()
             self.comment = comment
+            self.fees = Dollars(fees)
+            self.source = source
+            self.tokens_reported = 0
+            self.cost_basis = None
+
+        def compute_cost_basis(self):
+            source = self.source
+            tokens = self.tokens
+            date = self.date
+
+            if source and type(source) != list:
+                source = [source]
+
+            if tokens.in_tokens() > 0:
+                # this is a purchase
+                if not source:
+                    if self.proceeds == 0:
+                        warn(
+                            'purchase with no cost basis.',
+                            culprit = self.ident()
+                        )
+                    self.cost_basis = self.proceeds.add(self.fees)
+                    self.cost_basis_remaining = self.cost_basis
+                    #debug('PURCHASE', name=tokens.name(), date=date, cost_basis=str(self.cost_basis))
+                    return
+
+                if self.proceeds != 0:
+                    raise Error(
+                        'cannot specify both an price and a source on a purchase.',
+                        culprit = self.ident()
+                    )
+
+                basis = Dollars(0)
+                for src in source:
+                    try:
+                        src, fraction = src
+                    except TypeError:
+                        fraction = 1
+                    assert 0 <= fraction <= 1
+                    if src.cost_basis is None:
+                        raise Error(
+                            f'source transaction ({src.ident()}) has no cost basis.',
+                            culprit = self.ident()
+                        )
+                    if src.proceeds < 0:
+                        raise Error(
+                            f'using already reported asset ({src.ident()}) as source for purchase.',
+                            culprit = self.ident()
+                        )
+                    cost_basis_used = src.cost_basis*fraction
+                    basis = basis.add(cost_basis_used)
+                    src.cost_basis_remaining = src.cost_basis_remaining.add(-cost_basis_used)
+                self.cost_basis = basis.add(self.fees)
+                self.cost_basis_remaining = self.cost_basis
+                #debug('PURCHASE', self.ident(), tokens=self.tokens.in_tokens(), cost_basis=str(self.cost_basis))
+                return
+
+            # this is a sale
+            tokens_to_consume = -tokens.in_tokens()
+            basis = Dollars(0)
+            if not source:
+                source = []
+            for src in source:
+                if tokens.name() != src.tokens.name():
+                    raise Error(
+                        'token type differs from source',
+                        f'({src.tokens.name()}).',
+                        culprit = self.ident()
+                    )
+                if tokens_to_consume <= 0:
+                    warn(
+                        f'unneeded source ({src.tokens.name()} {src.date}).',
+                        culprit = self.ident()
+                    )
+                    break
+                tokens_available = src.tokens_remaining()
+                if tokens_to_consume <= tokens_available:
+                    # take only tokens we need
+                    tokens_consumed = tokens_to_consume
+                    tokens_to_consume = 0
+                else:
+                    # take all remaining tokens
+                    tokens_consumed = tokens_available
+                    tokens_to_consume -= tokens_available
+                src.tokens_reported += tokens_consumed
+                if tokens_consumed:
+                    cost_basis_used = src.cost_basis_remaining * (
+                        tokens_consumed / tokens_available
+                    )
+                else:
+                    cost_basis_used = 0
+                basis = basis.add(cost_basis_used)
+                src.cost_basis_remaining = src.cost_basis_remaining.add(-cost_basis_used)
+                #ddd(
+                #    'AFTER PILLAGE',
+                #    src.ident(),
+                #    cost_basis_remaining = str(src.cost_basis_remaining),
+                #    tokens_available = str(tokens_available),
+                #    tokens_reported = src.tokens_reported,
+                #    cost_basis_used = cost_basis_used,
+                #    tokens_consumed = str(tokens_consumed),
+                #    cost_basis_after = str(src.cost_basis),
+                #)
+
+            if tokens_to_consume > 0.001:
+                # 0.001 is assumed negligible
+                if source:
+                    msg = (
+                        'insufficient tokens available in',
+                        f'{plural(source):source}',
+                        f'(short by {tokens_to_consume}).'
+                    )
+                else:
+                    msg = (f'sale without a source.',)
+                warn(*msg, culprit = self.ident())
+            elif tokens_to_consume < -0.001:
+                raise AssertionError(f'overconsumed {tokens_to_consume}')
+            else:
+                self.cost_basis = basis.add(self.fees)
+                self.cost_basis_remaining = self.cost_basis
+
+            if self.proceeds:
+                self.profit = self.proceeds.scale(-1)
+                if self.cost_basis:
+                    self.profit = self.profit.add(-self.cost_basis)
+                #debug(profit=str(self.profit), proceeds=self.proceeds)
+            #debug('SALE', self.ident(), cost_basis=str(self.cost_basis))
 
         def fee(self):
-            return Quantity(self.cost - self.tokens.in_dollars(), '$')
+            return Dollars(self.cost - self.tokens.in_dollars())
 
-    def transaction(self, tokens, date=None, comment=''):
-        t = self.Transaction(tokens, date, comment)
-        self.transactions.append(t)
+        def reported(self):
+            return Dollars(self.tokens_reported * self.tokens.price)
+
+        def tokens_remaining(self):
+            return Quantity(
+                float(self.tokens) - self.tokens_reported,
+                self.tokens.tokens
+            )
+
+        def remaining(self):
+            return Dollars(self.tokens_remaining * self.tokens.price)
+
+        def ident(self):
+            with Quantity.prefs(spacer=''):
+                return ''.join(cull([self.tokens.name(), self.date])).lower()
+
+        def __str__(self):
+            return f'{self.date} {self.tokens}'
+
+    def transaction(self, tokens, date=None, comment='', fees=0, source=None):
+        t = self.Transaction(tokens, date, comment, fees, source)
+        ident = t.ident()
+        assert ident not in self.transactions, f'{ident} not unique'
+        self.transactions[ident] = t
         kind = tokens.name()
         num_tokens = float(tokens)
         self.totals[kind] = Quantity(
             num_tokens + self.totals.get(kind, 0), units=tokens.UNITS
         )
-        self.costs[kind] = Quantity(
-            t.cost + self.costs.get(kind, 0), units='$'
-        )
-        if t.cost > 0:
+        self.costs[kind] = Dollars(t.proceeds + self.costs.get(kind, 0))
+        if t.proceeds > 0:
             self.purchased[kind] = Quantity(
                 num_tokens + self.purchased.get(kind, 0), tokens.UNITS
             )
+        return t
+
+    def find_transaction(self, ident):
+        try:
+            return self.transactions[ident]
+        except KeyError:
+            matches = [k for k in self.transactions.keys() if k.startswith(ident)]
+            if len(matches) == 1:
+                return matches[0]
+            elif matches:
+                raise Error(
+                    "transaction identifier is not unique, matches:",
+                    *matches,
+                    sep='\n    ',
+                    culprit=ident,
+                )
+            raise Error('ident not found.', culprit=ident)
 
     def confirm_balance(self, kind, tokens):
         actual = self.totals[kind.__name__]
@@ -212,11 +390,11 @@ class Account:
                 total_dollars += token.scale('$')
             except UnknownConversion:
                 pass # this will get reported elsewhere
-        return Quantity(total_dollars, '$')
+        return Dollars(total_dollars)
 
     def total_cost(self):
         "Total cost of tokens purchased in dollars."
-        return Quantity(sum(self.costs.values()), '$')
+        return Dollars(sum(self.costs.values()))
 
     def largest_share(self):
         "Value of currency in dollars with the largest holdings."
@@ -228,5 +406,23 @@ class Account:
                     max_dollars = dollars
             except UnknownConversion:
                 pass # this will get reported elsewhere
-        return Quantity(max_dollars, '$')
+        return Dollars(max_dollars)
 
+    def gen_csv(self, filename):
+        import csv
+
+        with open(filename, 'w', newline='') as csvfile:
+            fieldnames = 'date coin amount price total comment'.split()
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for transaction in sorted(self.transactions.values(), key=lambda t: t.tokens.price):
+                data = dict(
+                    date = transaction.date,
+                    coin = transaction.tokens.name(),
+                    amount = float(transaction.tokens.in_tokens()),
+                    price = str(transaction.tokens.price),
+                    total = str(transaction.tokens.in_dollars()),
+                    comment = transaction.comment,
+                )
+                writer.writerow(data)
